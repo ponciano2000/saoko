793f41a6d8cd37ac73eb12f69d7a6aff7d406a8c
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-09-26 11:00:24 -0500

    ci:cracion de documento de java

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("generation").style.color = "grey";
+    console.log('Saoko papi saoko');
+    function colorTitulo (){
+      console.log('Pulsó el boton de generation');
+      document.getElementById("generation").style.color = "blue";
+    }
